668c162e94913eb8948fc0333f0536512bc0bb4d
buihuyhooang <buihuyhooang@gmail.com>
2026-07-06 17:40:02 -0700

    kien thuc co ban

---
+++ bien_kieu_du_lieu.py
@@ -0,0 +1,9 @@
+character_name = "Sáng"
+character_age = "40"
+print(f"Tôi là {character_name}")
+print(f"Tôi năm nay {character_age} tuổi")
+#f là viết tắt của format, nó giúp mình in ra các biến trong chuỗi mà không cần phải nối chuỗi bằng dấu cộng.
+#3 kiểu dữ liệu phổ biến 
+#string: "abc"
+#number: 123
+#boolean: True/False 
---
+++ nhap_du_lieu_nguoi_dung_vao.py
@@ -0,0 +1,9 @@
+name = input("Nhập tên của bạn: ")
+age = input ("Nhập tuổi của bạn: ")
+
+print("Xin chào, " + name)
+print("Bạn năm nay " + age + " tuổi.")
+
+print("Xin chào, " + name + ". Năm nay, bạn " + age + " tuổi.")
+
+print(f"Xin chào, {name}. Năm nay, bạn {age} tuổi.")
